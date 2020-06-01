9820028077f77acafdda6dd5d824c5c09a7f1b69
RafaelGoulartB <rafagoulartb@gmail.com>
2020-06-01 14:32:06 -0300

    feat: add structure files of header and fotter

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Fotter() {
+  return (
+    <footer>
+      <h1>Footer</h1>
+      <style jsx>{``}</style>
+    </footer>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <footer>
+      <h1>Header</h1>
+      <style jsx>{``}</style>
+    </footer>
+  );
+}
