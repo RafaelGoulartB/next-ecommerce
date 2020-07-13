71f8091237696af5929ea8946400c8e2146ba286
RafaelGoulartB <rafagoulartb@gmail.com>
2020-07-13 11:04:17 -0300

    🌟 feat: index and reset password front-end

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
 import Page from '../components/page';
 
-export default function Home() {
-  return (
-    <Page title="Quantum E-commerce - Products">
-      <div>
-        <h1>Home with Layout</h1>
-        <style jsx>{''}</style>
-      </div>
-    </Page>
-  );
+const ViewerQuery = gql`
+  query ViewerQuery {
+    viewer {
+      id
+      name
+      email
+    }
+  }
+`;
+
+export default function Index() {
+  const router = useRouter();
+  const { data, loading, error } = useQuery(ViewerQuery);
+  const viewer = data?.viewer;
+  const shouldRedirect = !(loading || error || viewer);
+
+  useEffect(() => {
+    if (shouldRedirect) {
+      router.push('/login');
+    }
+  }, [shouldRedirect]);
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  if (viewer) {
+    return (
+      <Page>
+        <p>You're signed in as {viewer.name}</p>
+        <Link href="/signout">
+          <a>signout</a>
+        </Link>
+      </Page>
+    );
+  }
+
+  return <p>Loading...</p>;
 }
---
+++ pages/resetpassword.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import PageContainer from '../components/page-container';
+import Link from 'next/link';
+import { getErrorMessage } from '../lib/form';
+
+import AlertError from '../components/alerts/error';
+import Button from '../components/form/button';
+import Input from '../components/form/input';
+import InputContainer from '../components/form/InputContainer';
+import FormContainer from '../components/form/formContainer';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [msgError, setMsgError] = useState('');
+
+  const router = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <PageContainer title="Quantum E-commerce - Reset Password">
+      <FormContainer>
+        <form onSubmit={handleSubmit}>
+          <h3 className="formTitle">Reset Password</h3>
+
+          {msgError && <AlertError message={msgError} />}
+
+          <InputContainer>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={(value) => setEmail(value)}
+              value={email}
+            />
+
+            <Button type="submit" title="Sent Email" />
+          </InputContainer>
+        </form>
+
+        <Link href="/signup">
+          <a className="switchForm">I do not have a account</a>
+        </Link>
+      </FormContainer>
+
+      <style jsx>{`
+        form {
+          width: 100%;
+          align-items: center;
+        }
+        form .formTitle {
+          text-align: center;
+          font-size: 38px;
+          font-weight: 1000;
+          letter-spacing: 1.65px;
+          color: #b2b2b2;
+          text-transform: uppercase;
+          margin-bottom: 84px;
+        }
+        .switchForm {
+          color: #b2b2b2;
+          margin-top: 12px;
+          font-weight: 500;
+        }
+      `}</style>
+    </PageContainer>
+  );
+}
