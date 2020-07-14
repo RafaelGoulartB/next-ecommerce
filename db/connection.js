a4628008fed46326b8d66fa7f627645b61aa5cf9
RafaelGoulartB <rafagoulartb@gmail.com>
2020-07-14 16:50:30 -0300

    🐛 fix: error to connect to database in production

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,10 @@
 const knex = require('knex');
 const configuration = require('../knexfile');
 
-const config =
-  process.env.NODE_ENV === 'test'
-    ? configuration.test
-    : configuration.development;
+let config;
+
+if (process.env.NODE_ENV === 'test') config = configuration.test;
+if (process.env.NODE_ENV === 'development') config = configuration.development;
+if (process.env.NODE_ENV === 'production') config = configuration.production;
 
 export const connection = knex(config);
