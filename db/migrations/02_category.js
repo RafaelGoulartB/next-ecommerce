6f816cb9744db948a8ad68a6023ba4de8a51e366
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-14 14:14:05 -0300

    📦 feat(db): icon name to table of category (#20)

--- db/migrations/02_category.js
+++ db/migrations/02_category.js
@@ -2,6 +2,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('category', function (table) {
     table.increments('id').primary();
     table.string('name').notNullable();
+    table.string('label').notNullable();
+    table.string('md_icon').notNullable();
     table.timestamp('created_at').defaultTo('now()').notNullable();
   });
 };
--- db/seeds/01_categories.js
+++ db/seeds/01_categories.js
@@ -5,16 +5,76 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('category').insert([
-        { id: 1, name: 'computers', created_at: Date.now() },
-        { id: 2, name: 'mac', created_at: Date.now() },
-        { id: 3, name: 'laptop', created_at: Date.now() },
-        { id: 4, name: 'keyboard', created_at: Date.now() },
-        { id: 5, name: 'components', created_at: Date.now() },
-        { id: 6, name: 'speaker', created_at: Date.now() },
-        { id: 7, name: 'smartphone', created_at: Date.now() },
-        { id: 8, name: 'tv', created_at: Date.now() },
-        { id: 9, name: 'videogame', created_at: Date.now() },
-        { id: 10, name: 'watch', created_at: Date.now() },
+        {
+          id: 1,
+          name: 'computers',
+          label: 'Computers',
+          md_icon: 'MdDesktopWindows',
+          created_at: Date.now(),
+        },
+        {
+          id: 2,
+          name: 'mac',
+          label: 'Apple Computers',
+          md_icon: 'MdDesktopMac',
+          created_at: Date.now(),
+        },
+        {
+          id: 3,
+          name: 'laptop',
+          label: 'Laptop',
+          md_icon: 'MdLaptop',
+          created_at: Date.now(),
+        },
+        {
+          id: 4,
+          name: 'keyboard',
+          label: 'Keyboards',
+          md_icon: 'MdKeyboard',
+          created_at: Date.now(),
+        },
+        {
+          id: 5,
+          name: 'components',
+          label: 'Computer Components',
+          md_icon: 'MdMemory',
+          created_at: Date.now(),
+        },
+        {
+          id: 6,
+          name: 'speaker',
+          label: 'Accessories',
+          md_icon: 'MdSpeaker',
+          created_at: Date.now(),
+        },
+        {
+          id: 7,
+          name: 'smartphone',
+          label: 'Cell Phone',
+          md_icon: 'MdSmartphone',
+          created_at: Date.now(),
+        },
+        {
+          id: 8,
+          name: 'tv',
+          label: 'TV & Video',
+          md_icon: 'MdTv',
+          created_at: Date.now(),
+        },
+        {
+          id: 9,
+          name: 'videogame',
+          label: 'Game Console',
+          md_icon: 'MdVideogameAsset',
+          created_at: Date.now(),
+        },
+        {
+          id: 10,
+          name: 'watch',
+          label: 'MdWatch',
+          md_icon: 'MdWatch',
+          created_at: Date.now(),
+        },
       ]);
     });
 };
