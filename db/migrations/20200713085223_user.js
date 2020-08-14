449cb20fdb4ca62dfb8ae4eb85bfbf1cd53b6c7f
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-14 08:48:35 -0300

    📦 update(db): add unique in email fields

--- db/migrations/20200713085223_user.js
+++ db/migrations/20200713085223_user.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('user', function (table) {
     table.string('id').primary();
     table.string('name').notNullable();
-    table.string('email').notNullable();
+    table.string('email').unique().notNullable();
     table.string('password').notNullable();
     table.string('createdAt').notNullable();
   });
