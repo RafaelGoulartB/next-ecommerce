7b71167cda3a24eedb4a40aaac08cc052a068138
RafaelGoulartB <rafagoulartb@gmail.com>
2020-08-31 09:04:39 -0300

    📦 feat: change sort in header bar products (#26)

--- components/headerBarProducts.js
+++ components/headerBarProducts.js
@@ -1,24 +1,59 @@
-import Link from 'next/link';
+import { sortProductSectionVar } from '../apollo/client/cache';
+import { useQuery } from '@apollo/client';
+import { SORT_PRODUCT_SECTION } from '../apollo/client/queries';
 
 export default function HeaderBarProducts() {
+  const { data } = useQuery(SORT_PRODUCT_SECTION);
+
+  function handlePopularProductsClick() {
+    sortProductSectionVar(['rating', 'DESC']);
+  }
+  function handleLowPriceProductsClick() {
+    sortProductSectionVar(['price', 'ASC']);
+  }
+  function handleHighPriceProductsClick() {
+    sortProductSectionVar(['price', 'DESC']);
+  }
+
   return (
     <div className="header">
       <div className="sort-list">
-        <Link href="#">
-          <a id="popular-products" className="active">
-            Popular products
-          </a>
-        </Link>
-        <Link href="#">
-          <a id="low-price" className="">
-            Low price
-          </a>
-        </Link>
-        <Link href="#">
-          <a id="high-price" className="">
-            High price
-          </a>
-        </Link>
+        <a
+          id="popular-products"
+          className={
+            data.sortProductSection[0] === 'rating' &&
+            data.sortProductSection[1] === 'DESC'
+              ? 'active'
+              : ''
+          }
+          onClick={handlePopularProductsClick}
+        >
+          Popular products
+        </a>
+        <a
+          id="low-price"
+          className={
+            data.sortProductSection[0] === 'price' &&
+            data.sortProductSection[1] === 'ASC'
+              ? 'active'
+              : ''
+          }
+          onClick={handleLowPriceProductsClick}
+        >
+          Low price
+        </a>
+        <a
+          id="high-price"
+          className={
+            data.sortProductSection[0] === 'price' &&
+            data.sortProductSection[1] === 'DESC'
+              ? 'active'
+              : ''
+          }
+          onClick={handleHighPriceProductsClick}
+        >
+          High price
+        </a>
       </div>
       <style jsx>{`
         .header {
@@ -52,6 +87,9 @@ export default function HeaderBarProducts() {
         .header .sort-list a.active {
           color: #1875f0;
           border-bottom: 2px solid #1875f0;
+        }
+        .header .sort-list a:hover {
+          cursor: pointer;
         }
         @media (max-width: 850px) {
           .header {
