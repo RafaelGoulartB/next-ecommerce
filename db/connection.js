c627ae4782cc1eb937326c1d324ec0d6f25823fd
RafaelGoulartB <rafagoulartb@gmail.com>
2020-09-09 18:18:48 -0300

    📦 feat: try to add production database (#39)

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,6 @@ let config;
 
 if (process.env.NODE_ENV === 'test') config = configuration.test;
 if (process.env.NODE_ENV === 'development') config = configuration.development;
-if (process.env.NODE_ENV === 'production') config = configuration.production;
+if (process.env.NODE_ENV === 'production') config = configuration.development;
 
-export const connection = knex(config);
+export const connection = knex(configuration.development);
