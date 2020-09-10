2f5e92bf411c3ea84b0724a1b1f92ec5a91f5adc
RafaelGoulartB <rafagoulartb@gmail.com>
2020-09-10 06:54:14 -0300

    📦 feat: try to add database at vercel (#39)

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,17 @@
 const knex = require('knex');
 const configuration = require('../knexfile');
-const path = require('path');
 
 let config;
 
 if (process.env.NODE_ENV === 'test') config = configuration.test;
 if (process.env.NODE_ENV === 'development') config = configuration.development;
-if (process.env.NODE_ENV === 'production') config = configuration.production;
-
-console.log(config);
+if (process.env.NODE_ENV === 'production')
+  config = {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/db.sqlite',
+    },
+    useNullAsDefault: true,
+  };
 
 export const connection = knex(config);
