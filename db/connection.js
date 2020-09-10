bc9dcb010ad09daa24440de5ddecabdedc7a8ea5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-09-10 06:23:24 -0300

    📦 feat: use env variable to sqlite_filename (#39)

--- db/connection.js
+++ db/connection.js
@@ -6,23 +6,8 @@ let config;
 
 if (process.env.NODE_ENV === 'test') config = configuration.test;
 if (process.env.NODE_ENV === 'development') config = configuration.development;
-if (process.env.NODE_ENV === 'production')
-  config = {
-    client: 'sqlite3',
-    connection: {
-      filename: '../db/db.sqlite',
-    },
-    migrations: {
-      directory: '../db/migrations',
-    },
-    seeds: {
-      directory: '../db/seeds',
-    },
-    useNullAsDefault: true,
-  };
+if (process.env.NODE_ENV === 'production') config = configuration.production;
 
-console.log(path.dirname(__filename));
-console.log(__filename);
 console.log(config);
 
 export const connection = knex(config);
