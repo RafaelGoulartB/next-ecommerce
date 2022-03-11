72c8376ea56382eb460a478428833367dd136f84
RafaelGoulartB <rafagoulartb@gmail.com>
2022-03-11 12:14:11 -0300

    test widget nuvidio

--- next.config.js
+++ next.config.js
@@ -3,4 +3,19 @@ module.exports = {
   images: {
     domains: ['m.media-amazon.com'],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://d0b4-18-228-40-209.ngrok.io/' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ]
+      }
+    ]
+  },
+  reactStrictMode: true,
 };
