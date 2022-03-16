cf2ce1263ed6659320314f698978f669f37de7a4
RafaelGoulartB <rafagoulartb@gmail.com>
2022-03-16 17:07:42 -0300

    test widget nuvidio

--- components/productItem.js
+++ components/productItem.js
@@ -29,7 +29,7 @@ export default function ProductSection({ id, name, rating, img_url, price }) {
 
       <div className="product-img-box">
         <Link href={`/product/${id}`}>
-          <img className="product-img" src="https://i.imgur.com/fRdrkI1.jpg" />
+          <img crossOrigin='anonymous' className="product-img" src="https://i.imgur.com/fRdrkI1.jpg" />
         </Link>
       </div>
 
